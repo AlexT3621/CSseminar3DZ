ae0b14043dd26d8326214304b9f573f9880e8803
Alex Belyaev <t433621@yandex.ru>
2022-07-19 09:40:18 +0300

    add второй вариант решения Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -5,8 +5,8 @@
 23432 -> да*/
 
 
-Console.Write("Введите число: ");
-string? number = Console.ReadLine();
+/*Console.Write("Введите число: ");
+string number = Console.ReadLine();
 
 void CheckingNumber(string number){
   if (number[0]==number[4] || number[1]==number[3]){
@@ -18,4 +18,10 @@ void CheckingNumber(string number){
 if (number!.Length == 5){
   CheckingNumber(number);
 }
-else Console.WriteLine($"Введи правильное число");
+else Console.WriteLine($"Введи правильное число");*/
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+string newNumber = "";
+for (int i = number.Length -1; i >=0; i--) {newNumber += number[i];}
+if (newNumber == number){Console.WriteLine($"Ваше число: {number} - палиндром.");}
+else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
